f9a13533db1381db079510fc36c047cc874dcf80
Scott Emmons <scotte@users.noreply.github.com>
2014-11-06 14:05:52 -0800

    Add hvac mode and temperature setting
    Mode can be set to off, heat, cool, auto (range).
    Temperature setpoint can be set (or range for auto/range).
    Updated README.

--- README.md
+++ README.md
@@ -7,18 +7,54 @@ Status
 ======
 
 * Displays current temperature, humidity, setpoint, and mode
+* Sets hvac mode - off, heat, cool, range (aka auto)
+* Sets temperature set point (or high/low for range/auto mode)
 * Caches user authentication token to ~/.cache/nestcontrol/token
 
 Usage
 =====
 
+Usage examples:
+
 ```
-$ ./nest.py
-usage: nest.py [-h] -u USERNAME -p PASSWORD [-s SERIAL]
-nest.py: error: argument -u/--username is required
+$ ./nest.py -h
+usage: nest.py [-h] -u USERNAME -p PASSWORD [-s SERIAL] [-d DEBUG]
+               [command [command ...]]
+
+Nest thermostat controller
+
+positional arguments:
+  command               commands - off, heat, cool, auto, or a set temperature
+                        in Fahrenheit. For example, to turn hvac off, use the
+                        command "off", or to turn on heat and set the
+                        temperature, "heat 72", or to just set the temperature
+                        "68". For auto, specify a temperature range (such as
+                        "68-72"). Commands are executed in the order provided,
+                        and note: don't enter the quotes.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -u USERNAME, --username USERNAME
+                        Nest username.
+  -p PASSWORD, --password PASSWORD
+                        Nest password.
+  -s SERIAL, --serial SERIAL
+                        Nest serial number (Default: first nest found).
+  -d DEBUG, --debug DEBUG
+                        Debug level. Higher is more (Default: 0).
 
 $ ./nest.py -u skipper@gilligan.isle -p minnow
-Temp: 71.8°F, Humidity: 51%, Set: 71.1°F, Mode: off
+Temp: 75.9°F, Humidity: 51%, Set: 72.0°F, Mode: off                     
+
+$ ./nest.py -u skipper@gilligan.isle -p minnow 70 heat
+Temp: 75.9°F, Humidity: 51%, Set: 70.0°F, Mode: heat
+
+$ ./nest.py -u skipper@gilligan.isle -p minnow 65-77 auto
+Temp: 75.9°F, Humidity: 51%, Set: 65.0°F - 77.0°F, Mode: range          
+
+$ ./nest.py -u skipper@gilligan.isle -p minnow 70 off
+Temp: 75.9°F, Humidity: 51%, Set: 70.0°F, Mode: off
+
 ```
 
 Thanks
@@ -27,7 +63,7 @@ Thanks
 Much of this is possible due to [pynest](https://github.com/smbaker/pynest). That project, unfortunately, hasn't been
 maintained in a couple of years and pull requests for additional features (such as turning hvac mode on/off) are likely
 to never be merged. Instead of forking pynest, I chose to rewrite it from scratch a bit cleaner - more modular, better
-functional compisition, using requests instead of urllib2, argparse instead of optparser, and following PEP8 guidelines.
+functional composition, using requests instead of urllib, argparse instead of optparser, and following PEP8 guidelines.
 
 License
 =======
--- nest.py
+++ nest.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python2.7
 
+# Nest thermostat controller
+#
+# https://github.com/scotte/nestcontrol
+# Licensed under GNU GPL 3.0. See the LICENSE.
+
 import argparse
 import calendar
 import json
 import os
+import re
 import requests
 import time
 
 
 urls = {'login': 'https://home.nest.com/user/login',
-        'status': '/v2/mobile/user'}
+        'status': '/v2/mobile/user',
+        'set': '/v2/put/shared'}
+modes = ['off', 'heat', 'cool', 'auto', 'range']
 tokendir = '%s/.cache/nestcontrol' % os.environ['HOME']
 tokenfile = 'token'
 tokenpath = '%s/%s' % (tokendir, tokenfile)
+tempre = re.compile('^[0-9\.]+$')
+rangere = re.compile('^[0-9\.]+-[0-9\.]+$')
 debug = 0
 
 
@@ -25,15 +35,37 @@ def fahr_to_cel(fahrenheit):
 
 
 def nest_post(session, url, data, headers=None):
-    return session.post(url, headers=headers, data=data)
+    debug2('POST:', url)
+    debug3('headers:', headers)
+    r = session.post(url, headers=headers, data=data)
+    debug2('http_status:', r.status_code)
+    debug3('headers:', r.headers)
+    if r.status_code != 200:
+        log('Error, nest API returned HTTP status %d' % r.status_code)
+        return None
+    debug3('data:', r.text)
+    return r
 
 
 def nest_get(session, url, data, headers=None):
-    return session.get(url, headers=headers, data=data)
+    debug2('POST:', url)
+    debug3('headers:', headers)
+    r = session.get(url, headers=headers, data=data)
+    debug2('http_status:', r.status_code)
+    debug3('headers:', r.headers)
+    if r.status_code != 200:
+        log('Error, nest API returned HTTP status %d' % r.status_code)
+        return None
+    debug3('data:', r.text)
+    return r
 
 
-def auth_url(auth, url):
+def auth_url_userid(auth, url):
     return "%s%s.%s" % (auth['url'], url, auth['userid'])
+
+
+def auth_url_serial(auth, url, serial):
+    return "%s%s.%s" % (auth['url'], url, serial)
 
 
 def auth_headers(auth):
@@ -42,14 +74,15 @@ def auth_headers(auth):
             'X-nl-protocol-version': '1'}
 
 
-def auth_nest_post(session, auth, url, data):
+def auth_nest_post_serial(session, auth, url, data, serial):
     headers = auth_headers(auth)
-    return nest_post(session, auth_url(auth, url), data, headers)
+    jsondata = json.dumps(data)
+    return nest_post(session, auth_url_serial(auth, url, serial), jsondata, headers)
 
 
-def auth_nest_get(session, auth, url, data=None):
+def auth_nest_get_userid(session, auth, url, data=None):
     headers = auth_headers(auth)
-    return nest_get(session, auth_url(auth, url), data, headers)
+    return nest_get(session, auth_url_userid(auth, url), data, headers)
 
 
 def login(session, username, password):
@@ -66,11 +99,7 @@ def login(session, username, password):
     debug1('Logging in')
     payload = {'username': args.username, 'password': args.password}
     r = nest_post(session, urls['login'], payload)
-    debug2('http_status:', r.status_code)
-    debug3('headers:', r.headers)
-
     data = r.json()
-    debug3('json data:', data)
 
     auth = dict()
     auth['url'] = data['urls']['transport_url']
@@ -97,11 +126,48 @@ def login(session, username, password):
 
 def get_status(session, auth):
     debug1('Getting status')
-    r = auth_nest_get(session, auth, urls['status'])
-    debug2('http_status:', r.status_code)
-    debug3('headers:', r.headers)
-    debug3('json data:', r.json())
+    r = auth_nest_get_userid(session, auth, urls['status'])
     return r.json()
+
+
+def do_temp(session, auth, fahrenheit, serial):
+    celsius = fahr_to_cel(float(fahrenheit))
+    debug1('Setting temp to %s (%0.1f)' % (fahrenheit, celsius))
+    data = {'target_change_pending': True,
+            'target_temperature': celsius}
+    auth_nest_post_serial(session, auth, urls['set'], data, serial)
+
+
+def do_range(session, auth, range, serial):
+    (lowf, highf) = range.split('-')
+    lowc = fahr_to_cel(float(lowf))
+    highc = fahr_to_cel(float(highf))
+    debug1('Setting temp to %s-%s (%0.1f-%0.1f)' % (lowf, highf, lowc, highc))
+    data = {'target_change_pending': True,
+            'target_temperature_low': lowc,
+            'target_temperature_high': highc}
+    auth_nest_post_serial(session, auth, urls['set'], data, serial)
+
+
+def do_mode(session, auth, mode, serial):
+    if mode == 'auto':
+        mode = 'range'
+    debug1('Setting mode to %s' % mode)
+    data = {'target_change_pending': True,
+            'target_temperature_type': mode}
+    auth_nest_post_serial(session, auth, urls['set'], data, serial)
+
+
+def do_command(session, auth, command, serial):
+    debug1('Executing command "%s"' % command)
+    if tempre.match(command):
+        do_temp(session, auth, command, serial)
+    elif rangere.match(command):
+        do_range(session, auth, command, serial)
+    elif command in modes:
+        do_mode(session, auth, command, serial)
+    else:
+        log('Ignoring unknown command "%s"' % command)
 
 
 def log(*args):
@@ -124,10 +190,12 @@ def debug3(*args):
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description="Nest thermostat controller")
-    argparser.add_argument('-u', '--username', required=True, help='Nest username')
-    argparser.add_argument('-p', '--password', required=True, help='Nest password')
-    argparser.add_argument('-s', '--serial', help='Nest serial number (Default: first nest found)')
-    argparser.add_argument('-d', '--debug', type=int, default=0, help='Debug level. Higher is more (Default: 0)')
+    argparser.add_argument('-u', '--username', required=True, help='Nest username.')
+    argparser.add_argument('-p', '--password', required=True, help='Nest password.')
+    argparser.add_argument('-s', '--serial', help='Nest serial number (Default: first nest found).')
+    argparser.add_argument('-d', '--debug', type=int, default=0, help='Debug level. Higher is more (Default: 0).')
+    argparser.add_argument('command', nargs='*', help='commands - off, heat, cool, auto, or a set temperature in Fahrenheit. For example, to turn hvac off, use the command "off", or to turn on heat and set the temperature, "heat 72", or to just set the temperature "68". For auto, specify a temperature range (such as "68-72"). Commands are executed in the order provided, and note: don\'t enter the quotes.')
+
     args = argparser.parse_args()
 
     debug = args.debug
@@ -137,13 +205,18 @@ if __name__ == '__main__':
     auth = login(session, args.username, args.password)
 
     data = get_status(session, auth)
-
     if args.serial is None:
         for structure in data['structure']:
             serial = data['structure'][structure]['devices'][0].split('.')[1]
             pass
     else:
         serial = args.serial
+
+    for command in args.command:
+        do_command(session, auth, command, serial)
+
+    if len(args.command) > 0:
+        data = get_status(session, auth)
 
     temp = cel_to_fahr(data['shared'][serial]['current_temperature'])
     humidity = data['device'][serial]['current_humidity']
